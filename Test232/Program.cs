6dff26743701e95a48283d325af2e6bd190b1ecd
Ho Pham Phu An <22520013@gm.uit.edu.vn>
2023-09-27 09:00:46 +0700

    first commit

--- Test232/Program.cs
+++ Test232/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace Test232
     {
         static void Main(string[] args)
         {
+            "hello AN NE ";
         }
     }
 }
